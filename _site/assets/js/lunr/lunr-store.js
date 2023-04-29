87b0fd91b01bfd3d48ff9743703e3e59a9b5dcce
Yi Heng <51170353+tyheng97@users.noreply.github.com>
2023-04-29 23:23:25 +0800

    changes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Post: TCP vs UDP",
+        "excerpt":"TCP vs UDP is of one the most common question to be asked in a SWE interview. In this post I will cover TCP and UDP in details and compare their differences. Lower Layers (klement: This Stack Exchange Thread was very useful in helping my understanding.) To understand the differences...","categories": ["Post"],
+        "tags": ["Networks"],
+        "url": "/posts/2023-04-29-my-new-post/",
+        "teaser": null
+      },{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/projects/2023-04-29-my-asdnew-post/",
+        "teaser": null
+      },]
